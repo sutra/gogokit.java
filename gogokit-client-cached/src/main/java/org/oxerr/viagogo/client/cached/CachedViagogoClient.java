008156024fd32b24eba521d1fb5c72d27a99c427
Joe Zhou <zhoushuqun@gmail.com>
2023-02-08 20:51:24 +0800

    Add log.

--- gogokit-client-cached/src/main/java/org/oxerr/viagogo/client/cached/CachedViagogoClient.java
+++ gogokit-client-cached/src/main/java/org/oxerr/viagogo/client/cached/CachedViagogoClient.java
@@ -1,5 +1,7 @@
 package org.oxerr.viagogo.client.cached;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.oxerr.viagogo.client.ViagogoClient;
 import org.oxerr.viagogo.client.cached.inventory.CachedSellerListingsService;
 import org.oxerr.viagogo.client.catalog.EventService;
@@ -9,6 +11,8 @@ import org.redisson.api.RedissonClient;
 
 public class CachedViagogoClient implements ViagogoClient {
 
+	private final Logger log = LogManager.getLogger();
+
 	private final ViagogoClient client;
 
 	private final CachedSellerListingsService cachedSellerListingsService;
@@ -16,6 +20,8 @@ public class CachedViagogoClient implements ViagogoClient {
 	private boolean deleteOnlyInCache;
 
 	public CachedViagogoClient(ViagogoClient client, RedissonClient redission) {
+		log.info("[CachedViagogoClient] init");
+
 		this.client = client;
 
 		this.cachedSellerListingsService = new CachedSellerListingsService(
