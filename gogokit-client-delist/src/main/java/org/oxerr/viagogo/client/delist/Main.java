6fbb0458daab39618a79d05d0db1e3df74010d67
Joe Zhou <zhoushuqun@gmail.com>
2023-02-13 00:18:38 +0800

    Shutdown executor service.

--- gogokit-client-delist/src/main/java/org/oxerr/viagogo/client/delist/Main.java
+++ gogokit-client-delist/src/main/java/org/oxerr/viagogo/client/delist/Main.java
@@ -1,8 +1,10 @@
 package org.oxerr.viagogo.client.delist;
 
 import java.io.IOException;
-import java.util.concurrent.Executor;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 import org.oxerr.viagogo.client.rescu.RescuViagogoClient;
 import org.oxerr.viagogo.model.request.inventory.SellerListingRequest;
@@ -11,20 +13,20 @@ import org.oxerr.viagogo.model.response.inventory.SellerListing;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Main {
+public class Main implements AutoCloseable {
 
 	private final Logger log = LoggerFactory.getLogger(getClass());
 
 	private final RescuViagogoClient client;
 
-	private final Executor executor;
+	private final ExecutorService executorService;
 
 	private final int pageSize;
 
 	public Main(String token, int pageSize) {
 		this.pageSize = pageSize;
 		this.client = new RescuViagogoClient(token);
-		this.executor = Executors.newFixedThreadPool(pageSize);
+		this.executorService = Executors.newFixedThreadPool(pageSize);
 	}
 
 	public void delist() {
@@ -41,13 +43,14 @@ public class Main {
 				log.error(e.getMessage());
 			}
 		} while (listings == null || listings.getTotalItems() > 0);
+
 	}
 
 	private void delist(PagedResource<SellerListing> listings) {
 		log.info("Left items: {}", listings.getTotalItems());
 
 		for (var item : listings.getItems()) {
-			this.executor.execute(() -> {
+			this.executorService.execute(() -> {
 				try {
 					this.client.getSellerListingService().deleteListingByExternalListingId(item.getExternalId());
 				} catch (IOException e) {
@@ -57,11 +60,30 @@ public class Main {
 		}
 	}
 
+	@Override
+	public void close() {
+		List<Runnable> tasksNeverCommencedExecution = null;
+
+		try {
+			if (!executorService.awaitTermination(this.pageSize, TimeUnit.SECONDS)) {
+				tasksNeverCommencedExecution = executorService.shutdownNow();
+			}
+		} catch (InterruptedException e) {
+			tasksNeverCommencedExecution = executorService.shutdownNow();
+			Thread.currentThread().interrupt();
+		}
+
+		if (tasksNeverCommencedExecution != null) {
+			log.warn("{} task(s) that never commenced execution.", tasksNeverCommencedExecution.size());
+		}
+	}
+
 	public static void main(String[] args) {
 		var token = args[0];
 		var pageSize = Integer.parseInt(args[1]);
-		var main = new Main(token, pageSize);
-		main.delist();
+		try (var main = new Main(token, pageSize)) {
+			main.delist();
+		}
 	}
 
 }
