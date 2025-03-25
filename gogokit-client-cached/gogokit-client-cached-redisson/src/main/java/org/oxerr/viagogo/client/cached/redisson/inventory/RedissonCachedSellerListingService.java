6a25e7288ad49dcfe3445a8d65b0b1eed75ff47c
Joe Zhou <zhoushuqun@gmail.com>
2025-03-25 23:01:28 +0800

    Make listedExternalIds thread-safe.

--- gogokit-client-cached/gogokit-client-cached-redisson/src/main/java/org/oxerr/viagogo/client/cached/redisson/inventory/RedissonCachedSellerListingService.java
+++ gogokit-client-cached/gogokit-client-cached-redisson/src/main/java/org/oxerr/viagogo/client/cached/redisson/inventory/RedissonCachedSellerListingService.java
@@ -12,6 +12,7 @@ import java.util.Optional;
 import java.util.Random;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executor;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
@@ -242,7 +243,7 @@ public class RedissonCachedSellerListingService
 			List<CompletableFuture<PagedResource<SellerListing>>> checkings
 		) {
 			this.externalIdToCacheName = externalIdToCacheName;
-			this.listedExternalIds = new HashSet<>();
+			this.listedExternalIds = ConcurrentHashMap.newKeySet();
 			this.tasks = tasks;
 			this.checkings = checkings;
 		}
